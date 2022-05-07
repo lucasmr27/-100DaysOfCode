90be106580f6b39ae4350e674dbc7f2c9bbe303a
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-05-07 00:24:32 -0300

    Alteração de teste linha 23

--- Day-003/Treasure_Island.py
+++ Day-003/Treasure_Island.py
@@ -20,6 +20,7 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/_____ /
 *******************************************************************************
 ''')
+
 print("Bem-vindo a ilha do tesouro!")
 print("Sua missão é achar o tesouro!")
 escolha1 = input('você encontrou um cruzamento para onde deseja ir? Digite "esquerda" ou "direita?" ').lower()
