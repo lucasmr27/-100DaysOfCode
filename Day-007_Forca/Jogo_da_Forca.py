1744d0be7e1ee06365f280f96ce1f14c986af96e
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-05-10 09:58:09 -0300

    Desafio3 checando se o jogador venceu

--- Day-007_Forca/Jogo_da_Forca.py
+++ Day-007_Forca/Jogo_da_Forca.py
@@ -14,12 +14,18 @@ display = []
 for letra in palavra_escolhida:
     display.append('_')
 print(display)
-# Solicitando letra para o jogador
-palpite = input('Digite uma letra: ').lower()
 
-# Checando a letra
-for posicao in range(len(palavra_escolhida)):
-    if palpite == palavra_escolhida[posicao]:
-        display[posicao] = palpite
-print(display)
+fim_de_jogo = False
+# Solicitando letra para o jogador
+while not fim_de_jogo:
+    palpite = input('Digite uma letra: ').lower()
+    # Checando a letra e substituindo lacunas
+    for posicao in range(len(palavra_escolhida)):
+        if palpite == palavra_escolhida[posicao]:
+            display[posicao] = palpite
+    print(display)
+
+    if '_' not in display:
+        fim_de_jogo = True
+        print('Parabéns, você venceu!!!')
 
