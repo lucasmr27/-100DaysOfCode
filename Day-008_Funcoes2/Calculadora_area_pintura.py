aa71a45404b23828de8c37d58f865cc328e1ca98
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-05-11 17:33:59 -0300

    Adicionado cálculo da quantidade de baldes de tinta

---
+++ Day-008_Funcoes2/Calculadora_area_pintura.py
@@ -0,0 +1,15 @@
+# Write your code below this line 👇
+def paint_calc(height, width, cover):
+    area = height * width
+    cans = int(area / coverage) + (area % coverage > 0)
+    print(f'Você precisará de {cans} latas.')
+
+
+# Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
