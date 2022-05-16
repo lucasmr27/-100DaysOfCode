de29fd0d4fb642abee059be8446dfbeccdc01f11
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-05-15 21:02:20 -0300

    Loop para jogador fazer suas jogadas

--- Day-011_BlackJack/blackjack.py
+++ Day-011_BlackJack/blackjack.py
@@ -5,8 +5,9 @@ lista_de_cartas = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 print(logo)
 
 
-def pegar_carta():
-    return random.choice(lista_de_cartas)
+def pegar_carta(cartas):
+    """Adiciona uma carta aleatória na lista de cartas da mão"""
+    cartas.append(random.choice(lista_de_cartas))
 
 
 def contar_pontos(cartas):
@@ -21,5 +22,20 @@ def contar_pontos(cartas):
     return pontos
 
 
-cartas_jogador = []
-cartas_dealer = []
+jogador = []
+pegar_carta(jogador)
+dealer = []
+pegar_carta(dealer)
+pegar_carta(dealer)
+continuar = True
+
+
+while continuar:
+    pegar_carta(jogador)
+    print(f"Suas cartas são: {jogador}, você tem {contar_pontos(jogador)} pontos.")
+    if contar_pontos(jogador) > 21:
+        print("você perdeu")
+        break
+    continuar = input("Aperte 's' para pegar outra carta ou 'n' para passar.") == "s"
+
+print(f"As cartas do Dealer são: {dealer}, ele tem {contar_pontos(dealer)} pontos.")
