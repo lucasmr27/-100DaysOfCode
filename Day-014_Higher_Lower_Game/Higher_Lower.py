1b1afb6eff110e5378a1dc6b7284b261f8c07be1
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-05-17 15:01:31 -0300

    Feito a função compare, e colocada em loop

--- Day-014_Higher_Lower_Game/Higher_Lower.py
+++ Day-014_Higher_Lower_Game/Higher_Lower.py
@@ -2,9 +2,30 @@ import random
 import art
 from data import data
 
+
+def compare(a):
+    global option_a
+    b = random.randint(0, len(data) - 1)
+    option_a = b
+    print(f'Compare A: {data[a]["name"]}, {data[a]["description"]}, from {data[a]["country"]}.')
+    print(art.vs)
+    print(f'Against B: {data[b]["name"]}, {data[b]["description"]}, from {data[b]["country"]}.')
+    if input('Who has more followers? Type "A" or "B": ').upper() == 'A':
+        return data[a]['follower_count'] > data[b]['follower_count']
+    else:
+        return data[a]['follower_count'] < data[b]['follower_count']
+
+
 option_a = random.randint(0, len(data) - 1)
+score = 0
 print(art.logo)
-print(f'Compare A: {data[option_a]["name"]}, {data[option_a]["description"]}, from {data[option_a]["country"]}.')
-print(art.vs)
-option_b = random.randint(0, len(data)-1)
-print(f'Against B: {data[option_b]["name"]}, {data[option_b]["description"]}, from {data[option_b]["country"]}.')
+while True:
+    if compare(option_a):
+        score += 1
+        print(art.logo)
+        print(f"You're right! Current score: {score}")
+
+        continue
+    else:
+        print(f"Sorry, that's wrong. Final score: {score}")
+    break
