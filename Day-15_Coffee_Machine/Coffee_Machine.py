af5981f7961af29d4ef45f8d3355e785549d5271
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-05-18 18:21:45 -0300

    corrigindo bugs na função check_money

--- Day-15_Coffee_Machine/Coffee_Machine.py
+++ Day-15_Coffee_Machine/Coffee_Machine.py
@@ -12,7 +12,6 @@ def check_resources(request):
     for ingredient in request:
         if request[ingredient] >= resources[ingredient]:
             return False
-    print('ok')
     return True
 
 
@@ -25,6 +24,10 @@ def insert_coin():
     return quarters*0.25 + dimes*0.10 + nickles*0.05 + pennies*0.01
 
 
+def check_money(cost):
+    return money <= cost
+
+
 resources = {
     "water": 300,
     "milk": 200,
@@ -34,17 +37,27 @@ money = 0.00
 
 print(MENU['espresso']['ingredients']['water'])
 
-
 while True:
     flavour = input("What would you like? (espresso/latte/cappuccino):").lower()
     if flavour in [x for x in MENU]:
-        check_resources(MENU[flavour]['ingredients'])
-        money += insert_coin()
+        if not check_resources(MENU[flavour]['ingredients']):
+            print('Sorry, not enough resources.')
+            continue
+        while check_money(MENU[flavour]['cost']):
+            money += insert_coin()
+            money = round(money, 2)
+            print(f'Total inserido: R${money}')
+            if check_money(MENU[flavour]['cost']):
+                if input('Dinheiro insuficiente, deseja inserir mais moedas? "s" ou "n"').lower() == 'n':
+                    print(f'Retire seu dinheiro: R${money}')
+                    money = 0.0
+                    break
+
     elif flavour == 'report':
         report()
         continue
     else:
         print("Invalid flavour")
         continue
-
+    print("Irá retornar")
     break
