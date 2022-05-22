c4914b03431aef8e6b2998e45519afd3d34f236f
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-05-22 18:55:24 -0300

    Fazendo as perguntas: OK

--- Day-017_QuizProject/main.py
+++ Day-017_QuizProject/main.py
@@ -1,8 +1,16 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
     question_bank.append(Question(question['text'], question['answer']))
 
-print(question_bank[2].answer)
+# TODO: asking the questions
+quiz = QuizBrain(question_bank)
+quiz.next_question()
+
+# TODO: checking if  the answer was correct
+
+
+# TODO: checking if we're the end of the quiz
