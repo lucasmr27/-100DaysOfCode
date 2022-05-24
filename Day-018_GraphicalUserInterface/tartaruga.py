b7b385e8dcb2f51440a3c45ffef5a91c2b263edd
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-05-23 21:46:01 -0300

    Feito o andar do bebado

--- Day-018_GraphicalUserInterface/tartaruga.py
+++ Day-018_GraphicalUserInterface/tartaruga.py
@@ -1,12 +1,35 @@
 from turtle import Turtle, Screen
+import random
 
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
-for x in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
+def poligono(lados, tamanho=100):
+    tim.color(random.choice(colours))
+    for _ in range(lados):
+        tim.forward(tamanho)
+        tim.right(-360/lados)
+
+
+def andar_bebado(passos=100, distancia=25):
+    for _ in range(passos):
+        tim.forward(distancia)
+        tim.color(random.choice(colours))
+        tim.right(random.choice([0, 90, -90, 180]))
+
+
+colours = ['lime', 'gold', 'red', 'blue', 'yellow', 'orange', 'purple', 'dark violet', 'deep pink', 'deep sky blue']
+tim = Turtle()
+tim.shape("turtle")
+tim.color("red")
+
+andar_bebado()
+# for lado in range(3, 10):
+#     poligono(lado)
+#
+# for _ in range(15):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
 
 screen = Screen()
 screen.exitonclick()
