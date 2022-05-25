9a5163c0d23b6f0cdc79037dbc4b1f744a5ce8aa
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-05-25 17:23:48 -0300

    Conclusão do projeto do dia 19

--- Day-019_HigherOrderFunctions/corrida.py
+++ Day-019_HigherOrderFunctions/corrida.py
@@ -1,17 +1,38 @@
 from turtle import Turtle, Screen
+import random
 
-
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="With turtle will win the race? Enter a color: ")
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+all_turtles = []
 
 for turtle_index in range(0, 6):
-    tim = Turtle(shape='turtle')
-    tim.color(colors[turtle_index])
-    tim.penup()
+    new_turtle = Turtle(shape='turtle')
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
     posicao = turtle_index * 30 - 75
-    tim.goto(x=-230, y=posicao)
+    new_turtle.goto(x=-230, y=posicao)
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor() > 220:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
 
 screen.exitonclick()
