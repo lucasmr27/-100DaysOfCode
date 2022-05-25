419508d119e510a63c392875d9dac46c0af8f447
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-05-25 16:32:48 -0300

    Melhorias nos movimentos

--- Day-019_HigherOrderFunctions/FuncoesOrdemSuperior.py
+++ Day-019_HigherOrderFunctions/FuncoesOrdemSuperior.py
@@ -4,29 +4,34 @@ tin = Turtle()
 screen = Screen()
 
 
-def move_up():
-    tin.setheading(90)
+def move_forwards():
     tin.forward(18)
 
 
-def move_down():
-    tin.setheading(270)
-    tin.forward(18)
+def move_backwards():
+    tin.forward(-18)
 
 
-def move_left():
-    tin.setheading(180)
-    tin.forward(18)
+def turn_left():
+    new_heading = tin.heading() + 10
+    tin.setheading(new_heading)
 
 
-def move_right():
-    tin.setheading(0)
-    tin.forward(18)
+def turn_right():
+    new_heading = tin.heading() - 10
+    tin.setheading(new_heading)
 
+
+def clear():
+    tin.clear()
+    tin.penup()
+    tin.home()
+    tin.pendown()
 
 screen.listen()
-screen.onkey(key="w", fun=move_up)
-screen.onkey(key="s", fun=move_down)
-screen.onkey(key="a", fun=move_left)
-screen.onkey(key="d", fun=move_right)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(clear, "c")
 screen.exitonclick()
