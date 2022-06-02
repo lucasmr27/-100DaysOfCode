135943ea6e85eff08988335272b6db272f05cee2
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-06-01 21:12:38 -0300

    Movimentação em andamento

--- Day-022_Pong/main.py
+++ Day-022_Pong/main.py
@@ -1,7 +1,17 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(width=800, height=800)
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+computer = Paddle()
+screen.update()
 
+
+screen.listen()
+
+screen.onkey(computer.go_up(), "up")
+screen.onkey(computer.go_down(), "up")
 screen.exitonclick()
