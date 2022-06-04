a8c30b403a26f5c928fdaa1ae7eab66d2de9bb67
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-06-03 21:45:54 -0300

    Ajustes nas raquetes, resolvido problema da movimetação das raquetes e criação da class Ball

--- Day-022_Pong/main.py
+++ Day-022_Pong/main.py
@@ -1,25 +1,25 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
-
-
+from ball import Ball
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
+
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
-screen.update()
+ball = Ball()
 
 screen.listen()
+screen.onkey(l_paddle.go_up, key="w")
+screen.onkey(l_paddle.go_down, key="s")
+screen.onkey(r_paddle.go_up, key="Up")
+screen.onkey(r_paddle.go_down, key="Down")
 
 game_is_on = True
 while game_is_on:
-    screen.onkey(r_paddle.go_up, key="w")
-    screen.onkey(r_paddle.go_down, key="s")
-    screen.onkey(l_paddle.go_up, key="Up")
-    screen.onkey(l_paddle.go_down, key="Down")
     screen.update()
 
 screen.exitonclick()
