6bf37209f64b5c862f426ca22b5aa1eee54749ea
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-06-04 20:52:10 -0300

    Detecção de colisão com as paredes e com as raquetes

--- Day-022_Pong/main.py
+++ Day-022_Pong/main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -20,6 +21,17 @@ screen.onkey(r_paddle.go_down, key="Down")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.05)
     screen.update()
+    ball.move()
+
+    # detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # needs to bounce
+        ball.bounce_y()
+
+    # detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 329 or ball.distance(l_paddle) < 50 and ball.xcor() < -329:
+        ball.bounce_x()
 
 screen.exitonclick()
