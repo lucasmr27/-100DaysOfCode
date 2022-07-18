2e2ec63008fa23d9badf6f2570bc9fa6d29305f6
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-07-17 22:48:52 -0300

    Abrindo arquivos

--- Day-024_Open_Read_Write_Files/main.py
+++ Day-024_Open_Read_Write_Files/main.py
@@ -1,3 +1,11 @@
-file = open('my_file.txt')
-contents = file.read()
-print(contents)
+# Abre o arquivo e fecha ao concluir os comandos
+# o parametro mode determina o que poderá fazer
+with open('my_file.txt', mode='r') as file:
+    contents = file.read()
+    print(contents)
+
+
+# file = open('my_file.txt')
+# contents = file.read()
+# print(contents)
+# file.close()
