619f263da5931b974ac5eceb2bfb05300132ca6b
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-07-19 22:45:09 -0300

    Data Analysis With Pandas

--- Day-025_CSV_Data/main_weather.py
+++ Day-025_CSV_Data/main_weather.py
@@ -38,11 +38,28 @@ import pandas
 # print(monday.condition)
 
 # Create a dataframe from scratch
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+# print(data)
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
+
 data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
 }
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
-print(data)
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
+print(data_dict)
 
