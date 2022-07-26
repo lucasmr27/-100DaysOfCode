01fbdb826604f888a85f396bea5c67708ee2bf15
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-07-25 23:54:56 -0300

    Jogo Finalizado

--- Day-025_CSV_Data/main.py
+++ Day-025_CSV_Data/main.py
@@ -15,7 +15,14 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
-
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -25,6 +32,3 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
         # t.write(state_data.state.item())
-
-
-screen.exitonclick()
